6e13f52f6b61509cb90fb95fd656aacb85507a18
G Ayodele <grace.dele@gmail.com>
2022-12-06 14:27:51 -0500

    Working model.py

---
+++ model.py
@@ -0,0 +1,110 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy() # create sqlalchemy instance 
+
+# assosiation tables for the many to many relationshipts
+users_budgets = db.Table(
+    "users_budgets",
+    db.Column("user_id", db.ForeignKey("users.user_id")),
+    db.Column("budget_id", db.ForeignKey("budgets.budget_id"))
+)
+
+budgets_categories = db.Table(
+    "budgets_categories",
+    db.Column("category_id", db.ForeignKey("categories.category_id")),
+    db.Column("budget_id", db.ForeignKey("budgets.budget_id"))
+)
+
+class User(db.Model):
+    """Users of the application"""
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    fname = db.Column(db.String(64), nullable=False)
+    lname = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    partner_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=True) # is the partners user id
+
+    # relationships
+    partner = db.relationship("User")
+    budgets = db.relationship("Budget", secondary="users_budgets", back_populates="users") # many to many to budgets
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+
+class Transaction(db.Model):
+    """Transactions from the user accounts"""
+    __tablename__ = "transactions"
+
+    transaction_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    bank_name = db.Column(db.String(64), nullable=False)
+    vendor = db.Column(db.String(64), nullable=False)
+    amount = db.Column(db.Numeric(6,2), nullable=False) 
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.category_id"), nullable=True)
+
+    # relationships
+    category = db.relationship("Category", back_populates="transactions") # one to one
+
+    def __repr__(self):
+        return f'<Transaction transaction_id={self.transaction_id} amount={self.amount}>'
+
+class Category(db.Model):
+    """User defined budget categories"""
+    __tablename__ = "categories"
+
+    category_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    category_name = db.Column(db.String(64), nullable=False)
+
+    # relationships
+    transactions = db.relationship("Transaction", back_populates="category") # one to one
+    budgets = db.relationship("Budget", secondary="budgets_categories", back_populates="categories") # many to many to budgets
+
+
+    def __repr__(self):
+        return f'<Category category_id={self.category_id} category_name={self.category_name}>'
+
+class Budget(db.Model):
+    """Budget categories defind by the user"""
+    __tablename__ = "budgets"
+
+    budget_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    max_amount = db.Column(db.Numeric(6,2), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.category_id"), nullable=False)
+    shared = db.Column(db.Boolean, nullable=False)
+
+    #relationships
+    categories = db.relationship("Category", secondary="budgets_categories", back_populates="budgets") # many to many to categories
+    users = db.relationship("User", secondary="users_budgets", back_populates="budgets")
+
+    def __repr__(self):
+        return f'<Budget budget_id={self.budget_id} category_name={self.category_name} max_amount={self.max_amount}>'
+
+def connect_to_db(flask_app, db_uri="postgresql:///easy", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
+
+# class User_Budget(db.Model):
+#     """User and Budget Association table"""
+#     __tablename__ = "user_budgets"
+
+#     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id",nullable=False))
+#     budget_id = db.Column(db.Integer, db.ForeignKey("budgets.budget_id",nullable=False))
+
+#     def __repr__(self):
+#         return f'<User_Budget user_id={self.user_id} budget_id={self.budget_id}>'
